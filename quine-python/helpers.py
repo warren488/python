6edac425dc8daef22f2d2f6682fb1ed6dc165621
warren488 <warren.s.488@gmail.com>
2017-07-24 05:39:37 -0400

    main app

--- quine-python/helpers.py
+++ quine-python/helpers.py
@@ -1,18 +1,24 @@
 
 
 def filter_input(minterms):
+    largest = -1
     minterms = minterms.split(',')
     for i in range(len(minterms)):
         minterms[i] = minterms[i].strip()
     organizedminterms = []
     invalidterms = []
     for term in minterms:
-        if term.isdigit() is False:
+        if (term.isdigit() is False):
+            invalidterms.append(term)
+        elif (int(term) < 0):
             invalidterms.append(term)
         elif organizedminterms.count(term) == 0:
             organizedminterms.append(term)
+            if int(term) > largest:
+                largest = int(term)
+
     
-    return [invalidterms, organizedminterms]
+    return [invalidterms, organizedminterms, largest]
 
 def initial_input_prompt(invalidterms, organizedminterms):
     if len(invalidterms) > 0:        
--- quine-python/main.py
+++ quine-python/main.py
@@ -1,4 +1,5 @@
 import sys
+import math
 from minterm import Minterm
 from helpers import filter_input, initial_input_prompt
 
@@ -10,6 +11,7 @@ def main():
 
     invalidterms = filtered[0]
     organizedminterms = filtered[1]
+    largest = filtered[2]
 
     choice = initial_input_prompt(filtered[0], filtered[1])
     print(choice)
@@ -19,6 +21,8 @@ def main():
             additions = input("enter list of terms")
             newly_filtered = filter_input(additions)
             invalidterms = newly_filtered[0]
+            if largest < filtered[2]:
+                largest = filtered[2]
             #below line causes duplication of terms
             organizedminterms += newly_filtered[1]
             choice = initial_input_prompt(invalidterms, organizedminterms)
@@ -26,16 +30,44 @@ def main():
             deletions = input("list which terms you would like to delete")
             newly_filtered = filter_input(deletions)
             invalidterms = newly_filtered[0]
+            if largest < filtered[2]:
+                largest = filtered[2]
             for i in newly_filtered[1]:
                 organizedminterms.remove(i)
             choice = initial_input_prompt(invalidterms, organizedminterms)
     
     binary_minterms = []
+    organized_binary_minterms = {}
+    number_of_digits = math.ceil(math.log10(largest)/math.log10(2))
+    print("number of bits ",number_of_digits)
+    largest_number_of_ones = -1
     for term in organizedminterms:
-        binary_minterms.append(Minterm(int(term)))
+        push_on = Minterm(int(term))
+        binary_minterms.append(push_on)
+        if push_on.getOnes() > largest_number_of_ones:
+            largest_number_of_ones = push_on.getOnes()
     
-    for min in binary_minterms:
-        print(min.getImp())
+    for i in range(largest_number_of_ones):
+        temp_ith_array = []
+        for min in binary_minterms:
+            if min.getOnes() == i:
+                temp_ith_array.append(min)
+        
+        if len(temp_ith_array) > 0:
+            organized_binary_minterms[i] = temp_ith_array
+
+    for i in organized_binary_minterms:
+        # The i in organized_binary_minterms just happened to be numbers because of use case, i seems to just take on the value of the key
+        print(i,":")
+        print(organized_binary_minterms[i][0].getImp())
+        print(organized_binary_minterms[i][1].getImp())
+
+    for i in range(largest_number_of_ones - 1):
+        if organized_binary_minterms.has_key(i) & organized_binary_minterms.has_key(i+1):
+            
+
+    
+        
 
 
 if __name__ == "__main__": main()
