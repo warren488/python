fa1c3fa282cba97a5e1e1ecf70dba34763076370
warren488 <warren.s.488@gmail.com>
2018-09-27 00:42:24 -0400

    large commit

--- flaskapp/app.py
+++ flaskapp/app.py
@@ -16,5 +16,9 @@ def post():
 def show(id):
     return 'you requested: '+id
 
+@app.route('/*')
+def notFound():
+    return 'Resource not found'
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0')
